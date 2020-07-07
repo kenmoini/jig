f170bccb0b1328eb52cd3dd4317c98a352127d17
Ken Moini <ken@kenmoini.com>
2020-07-06 21:14:00 -0400

    fix facade

--- app-src/app/Providers/AppServiceProvider.php
+++ app-src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
       Schema::defaultStringLength(191);
-      
+
       if (env('APP_ENV') !== 'local') {
         URL::forceScheme('https');
       }
