b3f207512e6d3e9f23c96df5a39d132410e9ec00
Ken Moini <ken@kenmoini.com>
2021-02-12 23:17:01 -0500

    move around things

--- app-src/resources/views/events/create.blade.php
+++ app-src/resources/views/events/create.blade.php
@@ -30,17 +30,6 @@
   @csrf
   <div class="pf-c-form__group">
     <div class="pf-c-form__group-label">
-      <label class="pf-c-form__label" for="event_title">
-        <span class="pf-c-form__label-text">Event Title</span>
-        <span class="pf-c-form__label-required" aria-hidden="true">&#42;</span>
-      </label>
-    </div>
-    <div class="pf-c-form__group-control">
-      <input class="pf-c-form-control" type="text" id="event_title" name="event_title" required />
-    </div>
-  </div>
-  <div class="pf-c-form__group">
-    <div class="pf-c-form__group-label">
       <label class="pf-c-form__label" for="event_workshop_id">
         <span class="pf-c-form__label-text">Event Workshop</span>
         <span class="pf-c-form__label-required" aria-hidden="true">&#42;</span>
@@ -53,6 +42,17 @@
           <option value="{{ $workshop->id }}">{{ $workshop->name }}</option>
         @endforeach
       </select>
+    </div>
+  </div>
+  <div class="pf-c-form__group">
+    <div class="pf-c-form__group-label">
+      <label class="pf-c-form__label" for="event_title">
+        <span class="pf-c-form__label-text">Event Title</span>
+        <span class="pf-c-form__label-required" aria-hidden="true">&#42;</span>
+      </label>
+    </div>
+    <div class="pf-c-form__group-control">
+      <input class="pf-c-form-control" type="text" id="event_title" name="event_title" required />
     </div>
   </div>
   <div class="" id="eventDetailsHolder">
