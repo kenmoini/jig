bbcd44e992a170862f1997c8f12d8d2c105176f2
Ken Moini <ken@kenmoini.com>
2021-02-14 19:54:11 -0500

    update user password checks to include env var and admin generated file

--- app-src/app/Http/Middleware/DefaultAdminUserCheck.php
+++ app-src/app/Http/Middleware/DefaultAdminUserCheck.php
@@ -19,11 +19,31 @@ class DefaultAdminUserCheck
      */
     public function handle($request, Closure $next)
     {
-        // Check for default password, redirect to force change password page...
-        if ( (Hash::check('Passw0rd1!', auth()->user()->password)) && ($request->route()->getName() !== 'panel.get.users.edit' && $request->route()->getName() !== 'panel.post.users.update')) {
-            //Session::flash('message', 'Default admin user must not have default password!');
-            return redirect()->route('panel.get.users.edit', [auth()->user()->id]);
+      // Check for default password, redirect to force change password page...
+      if ( (Hash::check('Passw0rd1!', auth()->user()->password)) && ($request->route()->getName() !== 'panel.get.users.edit' && $request->route()->getName() !== 'panel.post.users.update')) {
+        //Session::flash('message', 'Default admin user must not have default password!');
+        return redirect()->route('panel.get.users.edit', [auth()->user()->id]);
+      }
+
+      // Make sure a user doesn't have a password the same as the ENV VAR for ADMIN_USER_PASSWORD
+      if (!empty(env('ADMIN_USER_PASSWORD'))) {
+        if ( (Hash::check(env('ADMIN_USER_PASSWORD'), auth()->user()->password)) && ($request->route()->getName() !== 'panel.get.users.edit' && $request->route()->getName() !== 'panel.post.users.update')) {
+          return redirect()->route('panel.get.users.edit', [auth()->user()->id]);
         }
-        return $next($request);
+      }
+
+      // Make sure a user doesn't have a password the same as the admin generated password
+      try {
+        $gitChecksum = \File::get(storage_path('app/generated_admin_password'));
+        if (!empty(trim($gitChecksum))) {
+          if ( (Hash::check(trim($gitChecksum), auth()->user()->password)) && ($request->route()->getName() !== 'panel.get.users.edit' && $request->route()->getName() !== 'panel.post.users.update')) {
+            return redirect()->route('panel.get.users.edit', [auth()->user()->id]);
+          }
+        }  
+      } catch (\Exception $e) {
+        // No file found most likely, continue
+      }
+
+      return $next($request);
     }
 }
